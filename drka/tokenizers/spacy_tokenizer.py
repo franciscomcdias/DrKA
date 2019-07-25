bbadea138db4bf65bb4189c9afa57cf10fa484fd
Francisco Dias <francisco.dias@shell.com>
2019-07-25 23:48:16 +0200

    SpaCy entities

--- drka/tokenizers/spacy_tokenizer.py
+++ drka/tokenizers/spacy_tokenizer.py
@@ -28,7 +28,7 @@ class SpacyTokenizer(Tokenizer):
         if not any([p in self.annotators for p in ['lemma', 'pos', 'ner']]):
             nlp_kwargs['tagger'] = False
         if 'ner' not in self.annotators:
-            nlp_kwargs['entity'] = False
+            nlp_kwargs['ner'] = False
         self.nlp = spacy.load(model, **nlp_kwargs)
 
     def tokenize(self, text):
